4eea7a53d7157bffb85d88df52e3799e1531afe6
Danilo Henrique <daniloohenriquee@gmail.com>
2023-07-02 21:22:43 -0300

    Exam Complete

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
+import { TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
+
+interface Task {
+  id: number;
+  title: string;
+  done: boolean;
+}
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    console.log('clicou em addTask: ', newTaskTitle);
+
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(oldState => [...oldState, data]);
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+
+    const newTasks = tasks.map(task => {
+      if (task.id === id)
+        return {...task, done: !task.done }
+      
+      return task;      
+    })
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    //TODO - remove task from state    
+    setTasks(tasks => tasks.filter(
+      task => task.id !== id
+    ));
   }
 
   return (
